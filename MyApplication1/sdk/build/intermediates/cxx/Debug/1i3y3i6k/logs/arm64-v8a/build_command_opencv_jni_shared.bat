7f342f503fd968fba4739850c7c9eda1ed255d6e
hjee02018@naver.com <hjee02018@naver.com>
2023-11-15 21:45:59 +0900

    1115최신

--- MyApplication1/sdk/build/intermediates/cxx/Debug/1i3y3i6k/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\USER\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\USER\\source\\android\\BestPhoto\\MyApplication1\\sdk\\.cxx\\Debug\\1i3y3i6k\\arm64-v8a" ^
-  opencv_jni_shared
